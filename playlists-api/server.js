232752d6c44d498c685d7fe3ed59da148837969b
Perry <131920204+SamPotentier@users.noreply.github.com>
2024-10-25 15:44:29 +0200

    start last day

--- playlists-api/server.js
+++ playlists-api/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 const axios = require('axios');
 
 id = uuidv4();
-// Enregistrement de l'API dans Consul vraiment la oulala
+// Enregistrement de l'API dans Consul vraiment la oulala e
 axios.put('http://consul:8500/v1/agent/service/register', {
   Name: 'playlists-api',
   Address: getLocalIp(),
